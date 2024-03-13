8b262100bd39d90a3b642a976b404c7d74701071
georgelucas77 <sjp07770@gmail.com>
2024-03-12 23:47:27 -0300

    firstCommit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+let displayValue = "0"
+function updateDisplay() {
+    document.getElementById('display').textContent=displayValue
+}
+
+function appendToDisplay(value) {
+    if (displayValue === "0") {
+        displayValue = value
+    } else {
+        displayValue += value
+    }
+    updateDisplay()
+}
+
+function calculateResult() {
+    try {
+        displayValue=eval(displayValue).toString()
+    } catch (error) {
+        displayValue = "Error"
+        
+    }
+    updateDisplay()
+}
+
+function deleteLastDigit(){
+    displayValue = displayValue.slice(0, -1)
+    updateDisplay()
+}
+
+function clearDisplay(){
+    displayValue="0"
+    updateDisplay()
+}
+
